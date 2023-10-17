5d103fb23f5c3ab7af523708c29475369ab65373
shifasualeha <94359997+shifasualeha@users.noreply.github.com>
2023-10-16 22:36:54 -0700

    Add files via upload
    Navigation lab

---
+++ homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:navigation_lab/cardlist.dart';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Text and Button'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'this is a text',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20), // Adding some spacing between Text and Button
+            ElevatedButton(
+              onPressed: () {
+                // Code to execute when the button is pressed
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => MyApp())
+                );
+                Navigator.of(context).pop;
+
+                print('Button was pressed!');
+              },
+              child: Text('click here'),
+            ),
+          ],
+        ),
+     ),
+);
+}
+}
